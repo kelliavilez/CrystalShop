014bf8ef6fb50428c45d860e78cf519a4f263c82
Juliana <julianat1514@gmail.com>
2024-10-02 23:03:57 -0500

    App.tsx lo envolvimos en un appProvider
    Para que nos traiga el contexto

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { PaperProvider } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { AppProvider } from './src/context/AppContext'; // Importa el AppProvider
+import { PaperProvider } from 'react-native-paper';
 import LogIn from './src/screens/LogIn';
 import SignUp from './src/screens/SignUp';
 import Home from './src/screens/Home';
@@ -16,13 +17,10 @@ import Food from './src/screens/Categories/Food';
 import Gardening from './src/screens/Categories/Gargening';
 import Makeup from './src/screens/Categories/Makeup';
 import Pets from './src/screens/Categories/Pets';
-import ArticleDetailsCard from './src/components/ArticleDetailsCard';
 import ShoppingCart from './src/screens/ShoppingCart';
 import Profile from './src/screens/Profile';
 import Favorite from './src/screens/Favorite';
-import { Searchbar } from 'react-native-paper';
-import { useState } from 'react';
-import SearchBar from './src/components/SearchBar';
+import ArticleDetailsCard from './src/components/ArticleDetailsCard';
 import OfertsDetailsCard from './src/components/OfertsDetailsCard';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
@@ -44,25 +42,25 @@ const AuthenticationStack = () => ( //where the user can login or signup
 
 const AppDrawer = () => (
 
-  <Drawer.Navigator 
-  screenOptions={{
-    drawerStyle: {
-      backgroundColor: "#ebf7ec",
-      width: 240,
-    },
-    drawerActiveTintColor: "#447348", 
-    drawerInactiveTintColor: "#1e241e", 
-    drawerActiveBackgroundColor: "#b4dbb7", 
-    headerStyle: {
-      backgroundColor: "#ebf7ec", 
-    },
-    headerTintColor: "#1f3b28", 
-    headerTitleStyle: {
-      fontWeight: 'bold',
-    },
-  }}>
-    <Drawer.Screen name='Inicio' component={Home} 
-    options={{
+  <Drawer.Navigator
+    screenOptions={{
+      drawerStyle: {
+        backgroundColor: "#ebf7ec",
+        width: 240,
+      },
+      drawerActiveTintColor: "#447348",
+      drawerInactiveTintColor: "#1e241e",
+      drawerActiveBackgroundColor: "#b4dbb7",
+      headerStyle: {
+        backgroundColor: "#ebf7ec",
+      },
+      headerTintColor: "#1f3b28",
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    }}>
+    <Drawer.Screen name='Inicio' component={Home}
+      options={{
         drawerIcon: ({ size }) => (
           <Icon
             name={'home'}
@@ -71,114 +69,116 @@ const AppDrawer = () => (
           />
         ),
       }} />
-    <Drawer.Screen name='Perfil de usuario' component={Profile} 
-    options={{
-      drawerIcon: ({ size }) => (
-        <Icon
-          name={'person'}
-          size={size}
-          color={"#1f3b28"}
-        />
-      ),
-    }} />
+    <Drawer.Screen name='Perfil de usuario' component={Profile}
+      options={{
+        drawerIcon: ({ size }) => (
+          <Icon
+            name={'person'}
+            size={size}
+            color={"#1f3b28"}
+          />
+        ),
+      }} />
     <Drawer.Screen name='Mis favoritos' component={Favorite}
-    options={{
-      drawerIcon: ({ size }) => (
-        <Icon
-          name={'bookmark'}
-          size={size}
-          color={"#1f3b28"}
-        />
-      ),
-    }} />
+      options={{
+        drawerIcon: ({ size }) => (
+          <Icon
+            name={'bookmark'}
+            size={size}
+            color={"#1f3b28"}
+          />
+        ),
+      }} />
     <Drawer.Screen name='Mis compras' component={MyBoughts}
-    options={{
-      drawerIcon: ({ size }) => (
-        <Icon
-          name={'shopify'}
-          size={size}
-          color={"#1f3b28"}
-        />
-      ),
-    }} />
+      options={{
+        drawerIcon: ({ size }) => (
+          <Icon
+            name={'shopify'}
+            size={size}
+            color={"#1f3b28"}
+          />
+        ),
+      }} />
     <Drawer.Screen name='Ofertas' component={Oferts}
-    options={{
-      drawerIcon: ({ size }) => (
-        <Icon
-          name={'local-mall'}
-          size={size}
-          color={"#1f3b28"}
-        />
-      ),
-    }} />
+      options={{
+        drawerIcon: ({ size }) => (
+          <Icon
+            name={'local-mall'}
+            size={size}
+            color={"#1f3b28"}
+          />
+        ),
+      }} />
     <Drawer.Screen name='Pagos' component={Payment}
-    options={{
-      drawerIcon: ({ size }) => (
-        <Icon
-          name={'payments'}
-          size={size}
-          color={"#1f3b28"}
-        />
-      ),
-    }} />
+      options={{
+        drawerIcon: ({ size }) => (
+          <Icon
+            name={'payments'}
+            size={size}
+            color={"#1f3b28"}
+          />
+        ),
+      }} />
     <Drawer.Screen name='Ayuda y soporte' component={HelpSupport}
-    options={{
-      drawerIcon: ({ size }) => (
-        <Icon
-          name={'help'}
-          size={size}
-          color={"#1f3b28"}
-        />
-      ),
-    }} />
+      options={{
+        drawerIcon: ({ size }) => (
+          <Icon
+            name={'help'}
+            size={size}
+            color={"#1f3b28"}
+          />
+        ),
+      }} />
     <Drawer.Screen name='Categorias' component={ArticlesCategory}
-    options={{
-      drawerIcon: ({ size }) => (
-        <Icon
-          name={'category'}
-          size={size}
-          color={"#1f3b28"}
-        />
-      ),
-    }} />
+      options={{
+        drawerIcon: ({ size }) => (
+          <Icon
+            name={'category'}
+            size={size}
+            color={"#1f3b28"}
+          />
+        ),
+      }} />
     <Drawer.Screen name='Carrito de compras' component={ShoppingCart}
-    options={{
-      drawerIcon: ({ size }) => (
-        <Icon
-          name={'shopping-cart'}
-          size={size}
-          color={"#1f3b28"}
-        />
-      ),
-    }} />
+      options={{
+        drawerIcon: ({ size }) => (
+          <Icon
+            name={'shopping-cart'}
+            size={size}
+            color={"#1f3b28"}
+          />
+        ),
+      }} />
   </Drawer.Navigator>
 );
-
+// Componente principal de la app
 const App = () => (
-  <PaperProvider>
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name='Auth'
-          component={AuthenticationStack}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name='Main'
-          component={AppDrawer}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen name="ArticleDetailsCard" component={ArticleDetailsCard} />
-        <Stack.Screen name="OfertsDetailsCard" component={OfertsDetailsCard} />
-        <Stack.Screen name='Bedroom' component={Bedroom} />
-        <Stack.Screen name='Gardening' component={Gardening} />
-        <Stack.Screen name='Food' component={Food} />
-        <Stack.Screen name='Makeup' component={Makeup} />
-        <Stack.Screen name='Pets' component={Pets} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  </PaperProvider>
-
+  <AppProvider>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name='Auth'
+            component={AuthenticationStack}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name='Main'
+            component={AppDrawer}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="ArticleDetailsCard" component={ArticleDetailsCard} />
+          <Stack.Screen name="OfertsDetailsCard" component={OfertsDetailsCard} />
+          <Stack.Screen name='Bedroom' component={Bedroom} />
+          <Stack.Screen name='Gardening' component={Gardening} />
+          <Stack.Screen name='Food' component={Food} />
+          <Stack.Screen name='Makeup' component={Makeup} />
+          <Stack.Screen name='Pets' component={Pets} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  </AppProvider>
 );
 
 export default App;
+
