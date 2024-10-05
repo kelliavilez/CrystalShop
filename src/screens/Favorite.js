3b01c581723d4b3e73e1545084a04d928a8c403d
Juliana <julianat1514@gmail.com>
2024-10-04 21:08:49 -0500

    Favorite
    Organizamos las variables

--- src/screens/Favorite.js
+++ src/screens/Favorite.js
@@ -1,70 +1,28 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { View, FlatList } from 'react-native';
-import styles from '../styles/globalStyles';
-import FavoriteCard from "../components/FavoriteCard"; // Asegúrate de que la ruta sea correcta
+import FavoriteCard from "../components/FavoriteCard";
 import SearchBar from '../components/SearchBar';
-import { useState } from 'react';
+import { AppContext } from '../context/AppContext'; // Importamos el contexto global
 
 const Favorite = () => {
-  const [searchQuery, setSearchQuery] = useState('');
+    const { state } = useContext(AppContext); // Accedemos al contexto global para obtener los favoritos
+    const [searchQuery, setSearchQuery] = useState('');
 
-  const filteredArticles = articles.filter((article) =>
-    article.name.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+    // Filtramos los artículos favoritos según el valor ingresado en la barra de búsqueda
+    const filteredArticles = state.favorites.favoritesItems.filter((article) =>
+        article.name && article.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
 
-  return (
-    <View style={styles.viewStyle}>
-      <SearchBar onSearch={setSearchQuery} />
-      {filteredArticles.length > 0 ? (
-      <FlatList
-        data={filteredArticles}
-        renderItem={({ item }) => <FavoriteCard article={item} />} // Asegúrate de que el prop "ofert" es esperado en FavoriteCard
-        keyExtractor={(item) => item.id.toString()}
-      />
-    ) : (
-      <Text style={styles.subtitleLog}>No tienes nada en favoritos</Text>
-    )}
-    </View>
-  );
+    return (
+        <View style={{ flex: 1 }}>
+            <SearchBar onSearch={setSearchQuery} />
+            <FlatList
+                data={filteredArticles}
+                renderItem={({ item }) => <FavoriteCard article={item} />}
+                keyExtractor={(item) => item.id.toString()}
+            />
+        </View>
+    );
 };
-
-const articles = [
-  {
-    id: 42,
-    photo: 'https://catmajestic.com/wp-content/uploads/2023/04/Rascador-para-gatos-fingerprint-1000x1000.jpg',
-    name: 'Rascador Gatos',
-    description: 'Ideal para mantener las uñas de tu gato afiladas y evitar que rasguñe tus muebles.',
-    statusCategory: 'Disponible - Mascotas',
-    price: 200000,
-    category: 'Mascotas'
-  },
-  {
-    id: 43,
-    photo: 'https://cdnx.jumpseller.com/vive-rosa-vive-jardin1/image/29406919/resize/800/800?1668354944',
-    name: 'Macetas de cerámica',
-    description: 'Perfectas para plantar flores o hierbas en interiores o exteriores.',
-    statusCategory: 'Disponible - Jardineria',
-    price: 20000,
-    category: 'Jardineria'
-  },
-  {
-    id: 44,
-    photo: 'https://m.maccosmetics.com.co/media/export/cms/products/640x600/mac_sku_M66Y03_640x600_0.jpg',
-    name: 'Delineador waterproof',
-    description: 'Perfecto para una aplicación precisa y duradera durante todo el día.',
-    statusCategory: 'Disponible - Maquillaje',
-    price: 20500,
-    category: 'Maquillaje'
-  },
-  {
-    id: 45,
-    photo: 'https://http2.mlstatic.com/D_NQ_NP_713947-MCO45035432163_022021-O.webp',
-    name: 'Almohada ortopédica',
-    description: 'Diseñada para proporcionar soporte adecuado para el cuello y la cabeza.',
-    statusCategory: 'Disponible - Dormitorio',
-    price: 90000,
-    category: 'Dormitorio'
-  },
-];
 
 export default Favorite;
